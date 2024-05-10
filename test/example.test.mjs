e1cfbf15fb11d0696d5d5e99eb3ef4630bd1b701
jdeksnys <jonasdeksnys@gamil.com>
2024-05-10 18:32:19 +0300

    test

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,9 +1,15 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
 import { sum } from "../src/example.mjs";
+import { readContents } from "../src/app.mjs"
 
 describe("Example test fixture", () => {
-  test("Example test", () => {
+  test.skip("Example test", () => {
     expect(sum(1, 2)).to.equal(3);
   });
+
+  // open file
+  test("open file and read rle data", () =>{
+    expect(readContents("/Users/jonasdeksnys/Downloads/glider.rle.txt")).to.equal("bob$2bo$3o!");
+  })
 });
