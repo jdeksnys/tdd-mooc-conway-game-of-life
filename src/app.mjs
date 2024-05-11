8307c1960ce903b3fdf1f2835c29a02f52ac0bc0
jdeksnys <jonasdeksnys@gamil.com>
2024-05-11 20:17:06 +0300

    cli skeleton

--- src/app.mjs
+++ src/app.mjs
@@ -1,8 +1,31 @@
 import { readContents } from "./logic.mjs";
 
-// const args = process.argv.slice(2);
+
+function printUsage(){
+    console.error("\tUsage: node app.mjs <file_path> <number>");
+}
 
 export function importFile(filePath){
     let res = readContents(filePath);
     return res;
 }
+
+
+let filePath;
+let number;
+
+try{
+    const args = process.argv.slice(2);
+    filePath = args[0];
+    number = parseInt(args[1]);
+    if(args.length != 2 || !number){
+        printUsage();
+        process.exit(1);
+    }
+
+} catch {
+    printUsage();
+    process.exit(1);
+}
+
+console.log(`\t you entered: ${filePath} ${number}`);
