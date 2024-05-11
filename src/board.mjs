1e9fe10ce9a4c6d89386bbb53bdadc6a0f5652c8
jdeksnys <jonasdeksnys@gamil.com>
2024-05-11 09:25:11 +0300

    test-pass

--- src/board.mjs
+++ src/board.mjs
@@ -36,15 +36,33 @@ export class Board{
 		let shape = RleStrToArray(contents["data"]);
 		console.log(shape);
 		this.OverlayShapeToBoard(shape);
-
-		// for(let i=0; i<this.rows; i++){
-		// 	for(let j=0; j<this.cols; j++){
-		// 	}	
-		// }
-
 	}
 
 	OverlayShapeToBoard(shape) {
+		let board_centre_x = this.cols % 2 == 0
+			? Math.floor(this.cols/2) - 1
+			: Math.floor(this.cols/2);
+		let board_centre_y = this.rows % 2 == 0
+			? Math.floor(this.rows/2) - 1
+			: Math.floor(this.rows/2);
+		let shape_centre_x = shape[0].length % 2 == 0
+			? Math.floor(shape[0].length/2) - 1
+			: Math.floor(shape[0].length/2)
+		let shape_centre_y = shape.length % 2 == 0
+			? Math.floor(shape.length/2) - 1
+			: Math.floor(shape.length/2);
+
+		let shape_coord_x = board_centre_x - shape_centre_x;
+		let shape_coord_y = board_centre_y - shape_centre_y;
+		// console.log(shape_centre_x, shape_centre_y);
+
+
+		for(let i=0; i<shape.length; i++){
+			for(let j=0; j<shape[0].length; j++){
+				// console.log(shape[i][j]);
+			}	
+		}
+
 		this.matrix[1][3] = "o";
 		this.matrix[2][3] = "o";
 		this.matrix[3][3] = "o";
